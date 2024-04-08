a60eb2b033aea7ee8f5d65c68554732b340d57c1
Lesley C <lesley.yc@gmail.com>
2024-04-08 15:45:42 +0800

    first draft

---
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -0,0 +1,199 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  type IPropertyPaneConfiguration,
+  PropertyPaneTextField,
+  PropertyPaneCheckbox, PropertyPaneDropdown,
+  PropertyPaneToggle
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import type { IReadonlyTheme } from '@microsoft/sp-component-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+import styles from './HelloWorldWebPart.module.scss';
+import * as strings from 'HelloWorldWebPartStrings';
+import {
+  SPHttpClient, 
+  SPHttpClientResponse
+} from '@microsoft/sp-http'; 
+
+
+// properties here 
+export interface IHelloWorldWebPartProps {
+  description: string;
+  test: string; 
+  test1: boolean; 
+  test2: string; 
+  test3: boolean; 
+}
+
+export interface ISPLists {
+  value: ISPList[]; 
+}
+
+export interface ISPList {
+  Title: string; 
+  Id: string; 
+}
+
+export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
+
+  private _isDarkTheme: boolean = false;
+  private _environmentMessage: string = '';
+
+  public render(): void {
+    
+    this.domElement.innerHTML = `
+    <section class="${styles.helloWorld} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
+      <div class="${styles.welcome}">
+        <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
+        <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
+        <div>${this._environmentMessage}</div>
+        <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
+        <p>${escape(this.properties.test)}</p>
+        <p>${escape(this.context.pageContext.web.title)}</p>
+        <p>${this.properties.test1}</p>
+        
+        <p>${escape(this.properties.test2)}</p>
+        <p>${this.properties.test3}</p>
+      </div>
+      <div id="spListContainer" />
+    </section>`;
+  
+    this._renderListAsync()
+  }  
+  
+  private _renderListAsync(): void {
+    this._getListData().then((response) => {
+      this._renderList(response.value); 
+    })
+  }
+
+  private _getListData(): Promise<ISPLists> {
+    return this.context.spHttpClient.get(this.context.pageContext.web.absoluteUrl + `/_api/web/lists/getByTitle('Settings')/items?$select=Id,Title`, SPHttpClient.configurations.v1).then((response: SPHttpClientResponse) => {
+      return response.json()
+    })
+  }
+
+  private _renderList(items: ISPList[]): void {
+    let html: string = ''; 
+    items.forEach((item: ISPList) => {
+      html += `
+        <ul class="${styles.list}">
+          <li class="${styles.listItem}"> 
+            <span class="ms-font-1">${item.Title}</span>
+          </li>
+        </ul>
+      `;
+    });
+    const listContainer: Element | null = this.domElement.querySelector('#spListContainer');
+    
+    if (listContainer) {
+      listContainer.innerHTML = html;
+    } else {
+      console.error('Could not find the #spListContainer element.');
+    }
+  }
+
+  protected onInit(): Promise<void> {
+    return this._getEnvironmentMessage().then(message => {
+      this._environmentMessage = message;
+    });
+  }
+
+  private _getEnvironmentMessage(): Promise<string> {
+    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
+      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
+        .then(context => {
+          let environmentMessage: string = '';
+          switch (context.app.host.name) {
+            case 'Office': // running in Office
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
+              break;
+            case 'Outlook': // running in Outlook
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
+              break;
+            case 'Teams': // running in Teams
+            case 'TeamsModern':
+              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+              break;
+            default:
+              environmentMessage = strings.UnknownEnvironment;
+          }
+
+          return environmentMessage;
+        });
+    }
+
+    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    this._isDarkTheme = !!currentTheme.isInverted;
+    const {
+      semanticColors
+    } = currentTheme;
+
+    if (semanticColors) {
+      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
+      this.domElement.style.setProperty('--link', semanticColors.link || null);
+      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+    }
+
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                }),
+                PropertyPaneTextField('test', {
+                  label: 'Multi-line Text Field',
+                  multiline: true
+                }),
+                PropertyPaneCheckbox('test1', {
+                  text: 'Checkbox'
+                }),
+                PropertyPaneDropdown('test2', {
+                  label: 'Dropdown',
+                  options: [
+                    {
+                      key: '1', text: 'One'
+                    },
+                    { key: '2', text: 'Two' },
+                    {
+                      key: '3', text: 'Three'
+                    },
+                    {
+                      key: '4', text: 'Four'
+                    }
+                  ]
+                }),
+                PropertyPaneToggle('test3', {
+                  label: 'Toggle',
+                  onText: 'On',
+                  offText: 'Off'
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
